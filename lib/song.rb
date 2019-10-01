3cf0c54f39d06b7a5d02ff596c56e9871f060355
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-10-01 22:38:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   
   def self.find_or_create_by_name(song_name)
     found_song = self.find_by_name(song_name)
-    if found_song
+    if found_song(song_name)
       song
     else
       self.create_by_name(song_name)
